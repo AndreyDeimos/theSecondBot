e589a5eae3873e91f5b2175a2d200f0b03b4c256
Andrey <and.mi.eremenko@gmail.com>
2025-02-28 10:39:07 +0300

    a bit of changes
    I forgot to push them when I made them

--- db.py
+++ db.py
@@ -1,25 +1,47 @@
 import sqlite3
 
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
 
 def query_data(query: str, data=None):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
     if data is not None:
         cursor.execute(query, data)
     else:
         cursor.execute(query)
     result = cursor.fetchall()
     conn.commit()
-    conn.close()
     return result
 
 
 query_data("""
     CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY,
+        chat_id INTEGER PRIMARY KEY,
         first_name TEXT,
         second_name TEXT,
+        gender TEXT,
         role TEXT NOT NULL,
-        state TEXT NOT NULL
-    );
+        state TEXT
+    )
+""")
+
+query_data("""
+    CREATE TABLE IF NOT EXISTS competitions (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        date TEXT,
+        gender TEXT,
+        required_participants INTEGER
+    )
+""")
+
+query_data("""
+    CREATE TABLE IF NOT EXISTS temporary_competitions (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        date TEXT,
+        gender TEXT,
+        required_participants INTEGER,
+        user_id INTEGER
+    )
 """)
